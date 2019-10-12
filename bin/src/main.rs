89ea1c2d71d96eade816d91b3242ad2a2845221b
Paul Sajna <sajattack@gmail.com>
2019-10-11 21:14:46 -0700

    split into bin and lib

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,17 +1,14 @@
 extern crate byteorder;
 extern crate clap;
+extern crate uf2;
 
 use std::error::Error as ErrorTrait;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
-use byteorder::{LittleEndian, WriteBytesExt};
 use clap::{Arg, App};
-
-const UF2_MAGIC_START0: u32 = 0x0A324655; // "UF2\n"
-const UF2_MAGIC_START1: u32 = 0x9E5D5157; // Randomly selected
-const UF2_MAGIC_END: u32    = 0x0AB16F30; // Ditto
+use uf2::bin_to_uf2;
 
 fn main() -> Result<(), std::io::Error> {
     let matches = App::new("UF2Conv")
@@ -67,43 +64,3 @@ fn main() -> Result<(), std::io::Error> {
     Ok(())
 }
 
-fn bin_to_uf2(bytes: &Vec<u8>, family_id: u32, app_start_addr: u32) -> Result<Vec<u8>, std::io::Error> {
-    let datapadding = [0u8; 512-256-32-4];
-    let nblocks: u32 = ((bytes.len() + 255) / 256) as u32;
-    let mut outp: Vec<u8> = Vec::new();
-    for blockno in 0..nblocks {
-        let ptr = 256 * blockno;
-        let mut chunk = match bytes.get(ptr as usize..ptr as usize+256) {
-            Some(bytes) => bytes.to_vec(),
-            None => {
-                let mut chunk = bytes[ptr as usize..bytes.len()].to_vec();
-                while chunk.len() < 256 {
-                    chunk.push(0);
-                }
-                chunk
-            }
-        };
-        let mut flags: u32 = 0;
-        if family_id != 0 {
-            flags |= 0x2000
-        }
-
-        // header
-        outp.write_u32::<LittleEndian>(UF2_MAGIC_START0)?;
-        outp.write_u32::<LittleEndian>(UF2_MAGIC_START1)?;
-        outp.write_u32::<LittleEndian>(flags)?;
-        outp.write_u32::<LittleEndian>(ptr + app_start_addr)?;
-        outp.write_u32::<LittleEndian>(256)?;
-        outp.write_u32::<LittleEndian>(blockno)?;
-        outp.write_u32::<LittleEndian>(nblocks)?;
-        outp.write_u32::<LittleEndian>(family_id)?;
-
-        // data
-        outp.write(&chunk)?;
-        outp.write(&datapadding)?;
-
-        // footer
-        outp.write_u32::<LittleEndian>(UF2_MAGIC_END)?;
-    }
-    Ok(outp)
-}
