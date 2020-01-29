aaa1e321394d27802f0410035b5d69cf24a37e7d
Atul Bhosale <atul1bhosale@gmail.com>
2020-01-29 22:03:14 +0530

    Format code using 'cargo fmt'

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -6,15 +6,19 @@ use byteorder::{LittleEndian, WriteBytesExt};
 
 const UF2_MAGIC_START0: u32 = 0x0A324655; // "UF2\n"
 const UF2_MAGIC_START1: u32 = 0x9E5D5157; // Randomly selected
-const UF2_MAGIC_END: u32    = 0x0AB16F30; // Ditto
+const UF2_MAGIC_END: u32 = 0x0AB16F30; // Ditto
 
-pub fn bin_to_uf2(bytes: &Vec<u8>, family_id: u32, app_start_addr: u32) -> Result<Vec<u8>, std::io::Error> {
-    let datapadding = [0u8; 512-256-32-4];
+pub fn bin_to_uf2(
+    bytes: &Vec<u8>,
+    family_id: u32,
+    app_start_addr: u32,
+) -> Result<Vec<u8>, std::io::Error> {
+    let datapadding = [0u8; 512 - 256 - 32 - 4];
     let nblocks: u32 = ((bytes.len() + 255) / 256) as u32;
     let mut outp: Vec<u8> = Vec::new();
     for blockno in 0..nblocks {
         let ptr = 256 * blockno;
-        let chunk = match bytes.get(ptr as usize..ptr as usize+256) {
+        let chunk = match bytes.get(ptr as usize..ptr as usize + 256) {
             Some(bytes) => bytes.to_vec(),
             None => {
                 let mut chunk = bytes[ptr as usize..bytes.len()].to_vec();
@@ -48,4 +52,3 @@ pub fn bin_to_uf2(bytes: &Vec<u8>, family_id: u32, app_start_addr: u32) -> Resul
     }
     Ok(outp)
 }
-
